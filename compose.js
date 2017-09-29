b06d58a3f7c94b9aa6d2794c7a12c0f77517bd42
Justin Lee <justin.lee@ubc.ca>
2017-09-28 21:52:05 -0700

    wrote pipe

--- compose.js
+++ compose.js
@@ -3,8 +3,18 @@ const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
 const add5 = (x) => x + 5
 const multiply = (x, y) => x * y
 
-const multipleAndAdd5 = compose(
+const multipleAndAdd5C = compose(
     add5,
     multiply
 )
 
+console.log(multipleAndAdd5C(5, 2))
+
+const pipe = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)))
+
+const multiplyAndAdd5P = pipe(
+    multiply,
+    add5
+)
+
+console.log(multiplyAndAdd5P(5, 2))
