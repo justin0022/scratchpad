58c78ee452364800f1d120c89d3ea400490bffc7
Justin Lee <justinlee0022@gmail.com>
2017-10-04 08:59:50 -0700

    wrote mapreduce with composemap

--- mapReduce.js
+++ mapReduce.js
@@ -1,8 +1,16 @@
 const mapReduce = (mapFn) => (reduceFn) => (arr) => arr.map(mapFn).reduce(reduceFn)
 
+const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
+
 const reduceFn = (acc, cur) => (acc += cur)
 
 const mapFn = (x) => x * 2
 
 console.log(mapReduce(mapFn)(reduceFn)([1, 2, 3]))
 
+const composeFn = compose(
+    x => x * 2,
+    x => x - 1
+)
+
+console.log(mapReduce(composeFn)(reduceFn)([1, 2, 3]))
