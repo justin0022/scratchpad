bd70dc2d615329005ac7ea4b35a6e15462cbfdd0
Justin Lee <justinlee0022@gmail.com>
2017-10-04 17:11:25 -0700

    Fixed implementation of flattenArray

--- compose.js
+++ compose.js
@@ -1,7 +1,11 @@
 const add5 = (x) => x + 5
 const multiply = (x, y) => x * y
 
+// const multiplyArr = (ar) => ar.reduce((a, b) => (a *= b))
+
 const compose = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
+
+// fns.reduce((f, g) => (...args) => f(g(...args))) //=> (...args) => add5(multiply(...args))
 
 const multiplyAndAdd5 = compose(
     add5,
--- flattenArray.js
+++ flattenArray.js
@@ -1,7 +1,12 @@
 const input = [[1, 2, 3], [1, 2, 3, 4], [1], [1, 2, 3, 4, 5, 6]]
+// const input = [1, 2, 3, [4, 5, 6], 7, 8, 9]
 const nested = [[1, 2, 3], [1, 2, 3, 4], [1], [1, 2, 3, 4, 5, 6, [1, 2, 3]]]
 
-const flattenArray = (array) => array.reduce((acc, subArray) => [...acc, ...subArray])
+const flattenArray = (array) => array.reduce((acc, cur) => {
+  if (Array.isArray(cur)) {
+    return ([...acc, ...cur])
+  } else return ([...acc, cur])
+})
 
 console.log(flattenArray(input))
 console.log(flattenArray(nested))
