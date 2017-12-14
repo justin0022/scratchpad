ed264a02f2ee2f7301a42bf580c247d51bed164f
Justin Lee <justinlee0022@gmail.com>
2017-12-14 15:03:24 -0800

    Added average

---
+++ average.js
@@ -0,0 +1,3 @@
+const average = arr => arr.reduce((acc, val) => (acc + val)) / arr.length
+
+console.log(average([1, 2, 3, 4, 5]))
--- pipe.js
+++ pipe.js
@@ -1,11 +1,11 @@
+const pipe = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)))
+
 const add5 = (x) => x + 5
 const multiply = (x, y) => x * y
 
-const pipe = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)))
-
-const multiplyAndAdd5P = pipe(
+const multiplyAndAdd5 = pipe(
     multiply,
     add5
 )
 
-console.log(multiplyAndAdd5P(5, 2))
+console.log(multiplyAndAdd5(5, 2))
