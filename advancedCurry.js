0a9017411713bad26363627aa96c841c4559d43b
Justin Lee <justinlee0022@gmail.com>
2017-12-21 14:40:35 -0800

    werid default arguments

--- advancedCurry.js
+++ advancedCurry.js
@@ -1,4 +1,11 @@
-const testFunc = (add = (a, b) => a + b) => add
+// const testFunc = (add = (a, b) => a + b) => add
 
-console.log(testFunc()(1, 5))
-console.log(testFunc((a, b) => a - b)(1, 5))
+// console.log(testFunc()(1, 5))
+// console.log(testFunc((a, b) => a - b)(1, 5))
+
+const data = [1, 2, 3, 4, 5, 6]
+
+const filterWithDefault = (filteringFunc = x => x > 5) => data => data.filter(filteringFunc)
+
+console.log(filterWithDefault()(data)) // [6]
+console.log(filterWithDefault(x => x < 5)(data)) // [1, 2, 3, 4]
--- maxLength.js
+++ maxLength.js
@@ -5,9 +5,9 @@ const maxLength = arr => Math.max(...arr.map(x => x.length))
 console.log(maxLength(input))
 
 const zip = (...arrays) => {
-  const maxLength = Math.max(...arrays.map(x => x.length));
+  const maxLength = Math.max(...arrays.map(x => x.length))
   return Array.from({length: maxLength}).map((_, i) => {
-   return Array.from({length: arrays.length}, (_, k) => arrays[k][i]);
+    return Array.from({length: arrays.length}, (_, k) => arrays[k][i])
   })
 }
 
