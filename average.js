53e0e7efc014aef0b4cd0259c470ae3e154e481b
Justin Lee <justinlee0022@gmail.com>
2018-02-07 13:35:23 -0800

    Added default value to reduce in average so it can handle empty arrays

--- average.js
+++ average.js
@@ -1,3 +1,3 @@
-const average = arr => arr.reduce((acc, val) => (acc + val)) / arr.length
+const average = arr => arr.reduce((acc, val) => (acc + val), 0) / arr.length
 
 console.log(average([1, 2, 3, 4, 5]))
--- pipe.js
+++ pipe.js
@@ -1,6 +1,6 @@
 const pipe = (...fns) => (...args) => fns.reduce((f, g) => g(f(...args)))
 
-const add5 = (x) => x + 5
+const add5 = x => x + 5
 const multiply = (x, y) => x * y
 
 const multiplyAndAdd5 = pipe(
